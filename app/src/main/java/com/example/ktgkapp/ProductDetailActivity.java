5ae78dcbb9abff8d318118f20190ced9a994ae1f
thanh <thanh@LUCIFER>
2023-12-08 15:23:03 +0700

    detail update

--- app/src/main/java/com/example/ktgkapp/ProductDetailActivity.java
+++ app/src/main/java/com/example/ktgkapp/ProductDetailActivity.java
@@ -4,11 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ProductDetailActivity extends AppCompatActivity {
 
+    private int quantity = 0;
+    private double totalPrice = 0.0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +32,43 @@ public class ProductDetailActivity extends AppCompatActivity {
             nameTextView.setText(name);
             priceTextView.setText("Price: " + price);
         }
+        final TextView totalTextView = findViewById(R.id.totalTextView);
+        final Button buyButton = findViewById(R.id.buyButton);
+
+        buyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Xử lý sự kiện khi người dùng nhấn vào nút mua
+                quantity++;
+                totalPrice += getPriceFromIntent(); // Lấy giá từ intent hoặc từ đối tượng sản phẩm
+                updateTotalTextView(totalTextView);
+            }
+        });
+
+    }
+
+
+    private double getPriceFromIntent() {
+        Intent intent = getIntent();
+        if (intent != null) {
+            String priceString = intent.getStringExtra("price");
+            try {
+                return Double.parseDouble(priceString.replace("$", ""));
+            } catch (NumberFormatException e) {
+                e.printStackTrace();
+            }
+        }
+        return 0.0;
+    }
+
+    private void updateTotalTextView(TextView totalTextView) {
+        totalTextView.setText(String.format("Total: $%.2f", totalPrice));
+    }
+
+    public void onBuyButtonClick(View view) {
+        // Xử lý sự kiện khi người dùng nhấn vào nút mua (cách thức này được đặt trong thuộc tính onClick của nút mua)
+        quantity++;
+        totalPrice += getPriceFromIntent(); // Lấy giá từ intent hoặc từ đối tượng sản phẩm
+        updateTotalTextView((TextView) findViewById(R.id.totalTextView));
     }
 }
